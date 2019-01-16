b5e9a50b2a8136b052493f74577bbb53a64a5261
zhouyongchao <>
2019-01-16 11:36:07 +0800

    first commit

---
+++ Example/Pods/Target Support Files/Pods-ZYCBaseFramework_Tests/Pods-ZYCBaseFramework_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ZYCBaseFramework_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_ZYCBaseFramework_Tests
+@end
---
+++ Example/Pods/Target Support Files/ZYCBaseFramework/ZYCBaseFramework-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ZYCBaseFramework : NSObject
+@end
+@implementation PodsDummy_ZYCBaseFramework
+@end
